18a0da3778baa468a6c82f31a4002c72f59cf822
blha303 <stevensmith.ome@gmail.com>
2013-08-19 13:03:08 +0800

    Initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='terraria',
+      version='1.0',
+      description='Terraria ID lookup',
+      author='Steven Smith',
+      author_email='b3@blha303.com.au',
+      url='https://gist.github.com/blha303/6265817',
+      packages=['terraria'],
+     )
